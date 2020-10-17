946ad18f65220b9bb413b16e95df089460b8e963
Gaurav Krishan <gauravgupta@cse.iitb.ac.in>
2020-10-17 20:58:24 +0530

    changed folder

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl,FormGroup } from '@angular/forms';
+
+import { Form } from '../form123';
+import { FormService } from '../form.service';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+  data: Form;
+  msg: any;
+  flag;
+  constructor(private formService: FormService) { }
+  empForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    feedback: new FormControl(''),
+    comment: new FormControl(''),
+  });
+
+  onSubmit(){
+    this.formService.postConfig(this.empForm.value).subscribe(res => {console.log(res);this.msg='Form Successfully Submitted';this.flag=true}, error => {console.log(error);this.msg=JSON.stringify(error.error);this.flag=false});
+  }
+  ngOnInit() {
+    this.getdata();
+  }
+  getdata(): void {
+    this.formService.getConfig()
+    .subscribe(data => this.empForm.patchValue(data));
+  }
+}
